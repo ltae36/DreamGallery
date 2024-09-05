c2da19a51e8bdd0d2ea782d489539cf49580562e
Ltae <ltae36@gmail.com>
2024-09-05 18:32:34 +0900

    0905  멀티플레이

---
+++ Assets/Scripts/MultiSetting.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiSetting : MonoBehaviour
+{
+    public GameObject settingWindow;
+    public GameObject inputField;
+
+    void Start()
+    {
+        settingWindow.SetActive(false);
+        inputField.SetActive(false);
+    }
+
+    public void ClickBotton() 
+    {
+        settingWindow.SetActive(true);
+    }
+
+    public void EnterRoom() 
+    {
+        inputField.SetActive(true);
+    }
+}
--- Assets/Scripts/NetworkScripts/NetworkConnection.cs
+++ Assets/Scripts/NetworkScripts/NetworkConnection.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 using ExitGames.Client.Photon;
 using UnityEditor.XR;
 
-public class NetworkConnection : MonoBehaviour
+public class NetworkConnection : MonoBehaviourPunCallbacks
 {
     // 갤러리에서 온라인 모드를 켜면 방이 열리고 초대할 수 있음
     // 방은 자동으로 생성되고 보이게 할 지, 초대 할 지를 플레이어가 컨트롤
-    string enteredPassword = "사용자가 입력한 비밀번호"; // 사용자가 입력한 비밀번호를 저장하는 변수
+
+    /* 1. 마스터 서버에 입장한다 
+     * 2. 로비에 입장한다 
+     * 3. 방을 만들고 방 이름과 비밀번호를 설정한다
+     * 4. 방에 입장을 시도한다.
+     * 5. 실패했다면 로비로 돌아간다 */
+    public string enteredPassword = null; // 사용자가 입력한 비밀번호를 저장하는 변수
+    public string roomName;    
 
     void Start()
     {
@@ -18,65 +26,7 @@ public class NetworkConnection : MonoBehaviour
         PhotonNetwork.ConnectUsingSettings();
     }
 
-    void Update()
-    {
-
-    }
-
-    public void CreateRoom(string roomName, string password) 
-    {
-        RoomOptions roomOpt = new RoomOptions();
-        roomOpt.IsVisible = false; // 비공개 방 설정
-        roomOpt.IsOpen = true;
-        roomOpt.MaxPlayers = 20;
-        roomOpt.CustomRoomProperties = new ExitGames.Client.Photon.Hashtable() { { "Password", password } };
-        roomOpt.CustomRoomPropertiesForLobby = new string[] { "Password" };
-
-        PhotonNetwork.CreateRoom(roomName, roomOpt);
-    }
-    public void JoinRoomWithPassword(string roomName, string enteredPassword)
-    {
-        // 방 정보를 찾기 위한 사용자 정의 룸 속성 검색
-        PhotonNetwork.JoinRoom(roomName);
-    }
-
-    public void OnJoinRoomFailed(short returnCode, string message)
-    {
-        Debug.LogError("Failed to join room: " + message);
-    }
-
-    public void OnJoinRoom() 
-    {
-        print("성공적으로 방에 입장하였습니다!");
-
-        if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey("Password")) 
-        {
-            string roomPassword = PhotonNetwork.CurrentRoom.CustomProperties["Password"].ToString();
-            if(enteredPassword != roomPassword) 
-            {
-                print("비밀번호가 틀렸습니다!");
-                PhotonNetwork.LeaveRoom(); // 잘못된 번호 입력 시 방을 나감
-            }
-            else 
-            {
-                print("입장을 환영합니다!");
-            }
-        }
-    }
-
-    public void CreatePersistentRoom(string roomName, string password) 
-    {
-        RoomOptions roomOpt = new RoomOptions();
-        roomOpt.IsVisible = false;
-        roomOpt.IsOpen = true;
-        roomOpt.CustomRoomProperties = new ExitGames.Client.Photon.Hashtable() { { "Password", password } };
-        roomOpt.CustomRoomPropertiesForLobby = new string[] { "Password" };
-        roomOpt.EmptyRoomTtl = 0; // 방이 비어 있어도 제거되지 않음
-        roomOpt.PlayerTtl = -1; // 플레이어가 나가더라도 데이터 유지
-    }
-
-
-    /*// 마스터 서버에 접속이 되면 호출되는 함수
+    // 마스터 서버에 접속이 되면 호출되는 함수
     public override void OnConnectedToMaster()
     {
         base.OnConnectedToMaster();
@@ -89,7 +39,7 @@ public class NetworkConnection : MonoBehaviour
     public void JoinLobby()
     {
         // 닉네임 설정
-        PhotonNetwork.NickName = "태령";
+        PhotonNetwork.NickName = "꿈화가";
         // 기본 Lobby 입장
         PhotonNetwork.JoinLobby();
     }
@@ -100,21 +50,58 @@ public class NetworkConnection : MonoBehaviour
         base.OnJoinedLobby();
         print("로비 입장 완료");
 
-        JoinOrCreateRoom();
     }
 
     // Room 을 참여하자. 만약에 해당 Room 이 없으면 Room 만들겠다.
-    public void JoinOrCreateRoom()
+    public void CreateRoom()
     {
         // 방 생성 옵션
         RoomOptions roomOpt = new RoomOptions();
 
         roomOpt.MaxPlayers = 20; // 방에 들어올 수 있는 최대인원
-        roomOpt.IsVisible = true; // 로비에 방을 보이게 할 지 
+        roomOpt.IsVisible = false; // 로비에 방을 보이게 할 지 
         roomOpt.IsOpen = true; // 방에 참여할 수 있는지
+        roomOpt.CustomRoomProperties = new Hashtable()
+        {
+            // 비밀번호 설정
+            {"Password", enteredPassword}
+        };
+        // 로비에서 비밀번호를 확인할 수 있도록 설정
+        roomOpt.CustomRoomPropertiesForLobby = new string[] { "Password" };
 
         // Room 참여 or 생성
-        PhotonNetwork.JoinOrCreateRoom("meta_unity_room99", roomOpt, TypedLobby.Default);
+        PhotonNetwork.CreateRoom(roomName, roomOpt, TypedLobby.Default);
+    }
+
+    // 방에 입장할 때 비밀번호를 입력하게 하는 방법
+    public void JoinRoomWithPassword()
+    {
+        PhotonNetwork.JoinRoom(roomName);
+        // 암호를 입력받아서 암호가 맞다면 방에 입장한다. 아니라면 다시 입력한다.
+        if (PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey("Password"))
+        {
+            string roomPassword = PhotonNetwork.CurrentRoom.CustomProperties["Password"].ToString();
+            if (enteredPassword != roomPassword)
+            {
+                PhotonNetwork.LeaveRoom();
+            }
+            else
+            {
+                print("환영합니다!");
+            }
+        }
+    }
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.LogError("Failed to join room: " + message);
+    }
+
+    // 방 입장 성공했을 때 호출되는 함수
+    public override void OnJoinedRoom()
+    {
+        print("입장 대기 중!");
+        base.OnJoinedRoom();
+        
     }
 
     // 방 생성이 성공했을 때 호출되는 함수
@@ -123,15 +110,4 @@ public class NetworkConnection : MonoBehaviour
         base.OnCreatedRoom();
         print("방 생성 완료");
     }
-
-    // 방 입장 성공했을 때 호출되는 함수
-    public override void OnJoinedRoom()
-    {
-        base.OnJoinedRoom();
-        print("방 입장 완료");
-
-        // 멀티플레이 컨텐츠 즐길 수 있는 상태
-        // GameScene으로 이동
-        //PhotonNetwork.LoadLevel("GameScene");
-    }*/
 }

c035d0e531b768f0893c0caa87c484fb43495c10
FortressforTrees <rlarlcks08@naver.com>
2024-09-09 12:51:25 +0900

    .

--- Assets/Scripts/PaintingScripts/PlaneScript.cs
+++ Assets/Scripts/PaintingScripts/PlaneScript.cs
@@ -8,22 +8,27 @@ public class PlaneScript : MonoBehaviour
 
     public int paintedCount = 0;
     public int question1Count = 100;
+    public int question2Count = 200;
     public GameObject ChatUI;
     public Text question;
+    public GameObject exitChat;
 
     public float Timer = 10;
 
+
     bool question1 = true;
+    bool question2 = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+       
         if (paintedCount >= question1Count)
         {
             if (question1)
@@ -31,12 +36,25 @@ public class PlaneScript : MonoBehaviour
                 question1 = !question1;
                 ChatUI.SetActive(true);
                 question.text = "어떤 그림을 그리시나요? 얼마나 그리셨나요?";
+                StartCoroutine(ButtonOn());
+            }
+        }
+        if (paintedCount >= question2Count)
+        {
+            if (question2)
+            {
+                question2 = !question2;
+                ChatUI.SetActive(true);
+                question.text = "그림의 의도가 있으신가요?";
+                StartCoroutine(ButtonOn());
             }
         }
     }
 
     IEnumerator ButtonOn()
     {
-        yield return new WaitForSeconds(10);
+        yield return new WaitForSeconds(5);
+
+        exitChat.SetActive(true);
     }
 }
